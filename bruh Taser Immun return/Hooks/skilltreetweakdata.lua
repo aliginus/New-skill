c4d85ccc557cda867f2e98f9a9c6e697f8df28a5
ZE1S <sosikmix@gmail.com>
2025-11-08 22:39:13 +0300

    Update skilltreetweakdata.lua

--- bruh Taser Immun return/Hooks/skilltreetweakdata.lua
+++ bruh Taser Immun return/Hooks/skilltreetweakdata.lua
@@ -1,7 +1,8 @@
 Hooks:PostHook(SkillTreeTweakData, "init", "Skills_Buff", function(self)
 
 	self.skills.insulation[1].upgrades = {"player_taser_malfunction"}
-	self.skills.insulation[2].upgrades = {"player_taser_self_shock","player_escape_taser_1","player_resist_firing_tased"} --tier skills
+	self.skills.insulation[2].upgrades = {"player_taser_self_shock","player_escape_taser_1","player_resist_firing_tased"} 
 
 end)
+
 
