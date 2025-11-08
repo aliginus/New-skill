fad26d30fff0b4f5c6d42dab21621e5b7ea04884
ZE1S <sosikmix@gmail.com>
2025-11-08 22:30:44 +0300

    Update and rename Hooks to localizationmanager.lua

---
+++ localizationmanager.lua
@@ -0,0 +1,7 @@
+HooksAdd(LocalizationManagerPostInit, localizationmanager, function(loc)
+	LocalizationManageradd_localized_strings({
+
+    menu_insulation_beta_desc = " БАЗОВЫЙ ##$basic##nНаходясь под эффектом Тайзера есть ##30%## вернётся в него$NL;$NL;ПРО ##$pro;##$NL;Находясь под эффектом Тайзера вы можете освободиться от его эффекта при исспользовании кнопки взаимодействияnnКогда вы находитесь под эффектом Тайзера вы больше не тратите патроны  "
+})
+end)
+
